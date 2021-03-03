2e70d987982146cbba38622c64a85b86a6219962
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-03-03 20:12:50 +0000

    update index

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,11 +1,13 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
 
 app.get("/hello", (req, res, next) => {
-    console.log("Request");
+    console.log("--> Hello request");
     res.json({message: "Hello World!!"});
 });
 
-app.listen(8080, () => {
-    console.log("Server running on port 8080");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
